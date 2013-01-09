d4165746cebc76cb4fd62122a09f1acb910fa416
Daniel Hall <daniel.hall@rea-group.com>
2013-01-09 12:03:24 +1100

    Can grab data from authorative nameservers

---
+++ dnscheck.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl -w
+
+use Net::DNS::Resolver::Recurse;
+use Data::Dumper;
+
+my $domain = "realestate.com.au";
+my $host = "partner";
+my 
+
+
+my $fqdn = $host . "." . $domain;
+
+
+my $pres = Net::DNS::Resolver->new;
+$pres->tcp_timeout(2);
+$pres->udp_timeout(2);
+$pres->debug(0);
+ 
+my $packet = $pres->query($domain, "NS");
+
+foreach my $ns ($packet->answer) {
+  print "Checking: " . $ns->nsdname . "\n";
+  $pres->nameservers($ns->nsdname);
+  $answer = $pres->query($fqdn, "A");
+  foreach my $ip ($answer->answer) {
+    print "Got IP:" . $ip->address . "\n";
+  }
+}
