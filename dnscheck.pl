04fb7426c81caa23f6fa77c76c22eb262abef17b
Daniel Hall <daniel.hall@rea-group.com>
2013-01-11 10:42:22 +1100

    Check all the authorative servers and put the result in a hash on arrays

--- dnscheck.pl
+++ dnscheck.pl
@@ -9,9 +9,6 @@ my $domain = "realestate.com.au";
 my $host = "partner";
 my @validips = ("203.17.253.19", "195.43.154.19");
 
-
-
-
 # Code
 my $fqdn = $host . "." . $domain;
 
@@ -22,13 +19,17 @@ $pres->debug(0);
  
 my $packet = $pres->query($domain, "NS");
 
+my %answers = ();
 foreach my $ns ($packet->answer) {
-  print "Checking: " . $ns->nsdname . "\n";
   $pres->nameservers($ns->nsdname);
   $answer = $pres->query($fqdn, "A");
+  $answers{$ns->nsdname} = [];
   foreach my $ip ($answer->answer) {
-    if ( not grep $_ eq $ip->address, @validips ) {
-      print "ERROR got IP: " . $ip->address . "\n";
-    }
+    push ($answers{$ns->nsdname}, $ip->address);
   }
 }
+
+print Dumper(\%answers);
+
+
+
